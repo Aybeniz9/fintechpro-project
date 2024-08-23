0c65f28f02e3125b55f318d91f2d1831cbf7e07e
aybeniz <aybenizkazimova15@gmail.com>
2024-08-23 14:52:57 +0400

    test

---
+++ src/main/java/az/edu/turing/fintechproproject/FinTechProProjectApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.fintechproproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTechProProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinTechProProjectApplication.class, args);
+    }
+
+}
