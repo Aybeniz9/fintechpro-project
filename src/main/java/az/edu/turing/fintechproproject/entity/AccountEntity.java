929a8aa1b5669649a6f4e34e954bb1d3c72cd158
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-08-23 16:00:03 +0400

    AccountEntity , AccountStatus created

---
+++ src/main/java/az/edu/turing/fintechproproject/entity/AccountEntity.java
@@ -0,0 +1,40 @@
+package az.edu.turing.fintechproproject.entity;
+
+import az.edu.turing.fintechproproject.model.enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "Account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_number", nullable = false, unique = true, length = 16)
+    private String accountNumber;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_status", nullable = false)
+    private AccountStatus accountStatus;
+
+    @Column(name = "pin", length = 4)
+    private String pin;
+
+    @Column(name = "cvv", length = 3)
+    private String cvv;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
