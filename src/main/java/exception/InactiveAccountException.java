b38dd8c825022f604f6a3030b070cba696fe5c30
aybeniz <aybenizkazimova15@gmail.com>
2024-08-24 20:38:57 +0400

    Fix exception handling in application startup

---
+++ src/main/java/exception/InactiveAccountException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class InactiveAccountException extends RuntimeException {
+    public InactiveAccountException(String message) {
+        super(message);
+}
+}//Thrown when a user tries to interact with an inactive account (e.g., during a transfer).
